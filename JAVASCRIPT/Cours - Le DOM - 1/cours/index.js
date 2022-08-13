625025cca4a9877752a901db10ce67bf4d574fc9
last <63156389+lau-last@users.noreply.github.com>
2022-08-13 22:27:35 +0200

    cours JS 02:02:52

---
+++ JAVASCRIPT/Cours - Le DOM - 1/cours/index.js
@@ -0,0 +1,90 @@
+// SELECTEURS
+// document. querySelector("h4"). style. background = "yellow";
+// const baliseHTML= document. querySelector("h4");
+//-----------------------------------------------------
+// Click event
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
+
+questionContainer.addEventListener("click", () => {
+  questionContainer.classList.toggle("question-clicked");
+});
+
+btn1.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "green";
+});
+
+btn2.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "red";
+});
+// #id > .class > baliseHTML
+//-----------------------------------------------------
+// Mouse Events
+const mousemove = document.querySelector(".mousemove");
+
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousedown", () => {
+  mousemove.style.transform = "scale(2) translate(-25%,-25%)";
+});
+
+window.addEventListener("mouseup", () => {
+  mousemove.style.transform = "scale(1) translate(-50%,-50%)";
+  mousemove.style.border = "2px solid teal";
+});
+
+questionContainer.addEventListener("mouseenter", () => {
+  questionContainer.style.background = "rgba(0,0,0,0.6)";
+});
+
+questionContainer.addEventListener("mouseout", () => {
+  questionContainer.style.background = "pink";
+});
+
+response.addEventListener("mouseover", () => {
+  response.style.transform = "rotate(2deg)";
+});
+// -----------------------------------------------------
+// KeyPress event
+const keypressContainer = document.querySelector(".keypress");
+const key = document.getElementById("key");
+
+const ring = (key) => {
+  const audio = new Audio();
+  audio.src = key + ".mp3";
+  audio.play();
+};
+
+document.addEventListener("keypress", (e) => {
+  key.textContent = e.key;
+
+  if (e.key === "j") {
+    keypressContainer.style.background = "pink";
+  } else if (e.key === "h") {
+    keypressContainer.style.background = "teal";
+  } else {
+    keypressContainer.style.background = "red";
+  }
+  ring("./Enter");
+});
+// -----------------------------------------------------
+// Scroll Event
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+    //   console.log(window.scrollY);
+    
+    if (window.scrollY > 120) {
+        nav.style.top = 0;
+    } else {
+        nav.style.top = "-50px";
+    }
+});
+// -----------------------------------------------------
