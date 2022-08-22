7ad9418d06626420f10533cb50268f6944b496a3
last <63156389+lau-last@users.noreply.github.com>
2022-08-22 21:13:00 +0200

    TP7

--- JAVASCRIPT/TP7 - Présentation - Compte à Rebours/index.js
+++ JAVASCRIPT/TP7 - Présentation - Compte à Rebours/index.js
@@ -1,10 +1,33 @@
-// Créer la logique de compte à rebours
+const form = document.getElementById("form");
+const choice = document.getElementById("choice");
+const countdownDisplay = document.getElementById("countdownDisplay");
 
-choice.value = 5 minutes
+let totalSeconds;
+let interval;
 
-choice.value * 60 = totalSeconds
+function countDown() {
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+  let sec = seconds < 10 ? "0" + seconds : seconds;
 
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = totalSeconds % 60
+  countdownDisplay.textContent = `${minutes} : ${sec}`;
 
-// Créer un événement à la validation du form pour lancer le compte à rebours
+  if (totalSeconds > 0) {
+    totalSeconds--;
+  } else {
+    countdownDisplay.textContent = "C'est terminé !";
+    clearInterval(interval);
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (isNaN(choice.value)) {
+    alert("Entrez un chiffre avant que je me fâche");
+  } else {
+    totalSeconds = choice.value * 60;
+    choice.value = "";
+    clearInterval(interval);
+    interval = setInterval(countDown, 1000);
+  }
+});
