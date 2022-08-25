8d7d73c95094319421000a6843f15fbd1d6d82c9
last <63156389+lau-last@users.noreply.github.com>
2022-08-25 22:47:22 +0200

    Début du cours (01:00:17)

---
+++ REACT/cours-react-1/src/pages/About.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A PROPOS</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit
+        aliquid accusamus doloremque sapiente id harum odit explicabo labore
+        deserunt quod ipsam maiores pariatur ut modi officiis sunt dolores
+        fugit, temporibus illum quisquam necessitatibus ab sit! Ut, ullam
+        officia? Excepturi voluptatum tempora maiores ullam vitae ratione porro
+        nostrum! Voluptatum ab iure mollitia, dolorum reiciendis ex quae esse id
+        ut officiis rerum aliquid voluptas ea consectetur laborum, repellendus
+        aspernatur minima vero possimus magni, sed quam molestiae sequi. Modi,
+        itaque cupiditate? Beatae, iure incidunt? Optio nulla ut magnam
+        molestias, tenetur eaque, rem numquam quibusdam provident sunt
+        cupiditate voluptates tempore ipsam minima vero ducimus.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto dolorem
+        sequi minus amet ipsa accusantium ab, tempora, quibusdam, rerum sint
+        molestiae reiciendis optio adipisci velit nostrum ad expedita. Mollitia
+        quibusdam iure molestiae eius quisquam dolorem ullam atque sit libero
+        placeat asperiores cupiditate possimus corrupti, quas perspiciatis,
+        saepe assumenda nesciunt voluptate quaerat. Voluptas, totam quos autem
+        molestiae ut maiores? Illum eveniet hic voluptatem non consequuntur
+        ullam quia possimus, eos earum. Veritatis iusto ratione impedit, unde
+        ipsum maxime maiores eius minima amet asperiores earum voluptatem,
+        pariatur nam. Nihil sed provident repellendus voluptatum, atque fuga
+        similique, harum quas eligendi minus tempora, perspiciatis sint.
+      </p>
+    </div>
+  );
+};
+
+export default About;
