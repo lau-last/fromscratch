2ec9cedacec3e113a078f163599dc98f32807cd4
last <63156389+lau-last@users.noreply.github.com>
2022-08-29 20:57:35 +0200

    App site vitrine 58 minutes et 48 secondes

---
+++ REACT/app01-site-vitrine/src/components/DynamicText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DynamicText = () => {
+    return (
+        <div>
+            <h2>TEXT</h2>
+        </div>
+    );
+};
+
+export default DynamicText;
---
+++ REACT/app01-site-vitrine/src/components/SocialNetwork.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from "react";
+
+const SocialNetwork = () => {
+  useEffect(() => {
+    const icons = document.querySelectorAll(".social-network a");
+    icons.forEach((icon) => {
+      icon.addEventListener("mouseover", (e) => {
+        icon.style.transform = `translate(${e.offsetX - 20}px, ${
+          e.offsetY - 13
+        }px)`;
+      });
+      icon.addEventListener("mouseleave", (e) => {
+        icon.style.transform = "";
+      });
+    });
+  });
+  return (
+    <div className="social-network">
+      <ul className="content">
+        <a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover"
+        >
+          <li>
+            <i className="fab fa-facebook-f"></i>
+          </li>
+        </a>
+        <a
+          href="https://www.twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover"
+        >
+          <li>
+            <i className="fab fa-twitter"></i>
+          </li>
+        </a>
+        <a
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover"
+        >
+          <li>
+            <i className="fab fa-instagram"></i>
+          </li>
+        </a>
+      </ul>
+    </div>
+  );
+};
+
+export default SocialNetwork;
